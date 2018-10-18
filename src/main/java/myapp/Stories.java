383e75080bbc731e408fff6c7f5cc5d1035d27d1
anu-venu <44260825+anu-venu@users.noreply.github.com>
2018-10-18 16:05:31 +0530

    Update and rename DemoServlet.java to Stories.java

--- src/main/java/myapp/Stories.java
+++ src/main/java/myapp/Stories.java
@@ -15,17 +15,19 @@
  */
 
 package myapp;
-
 import java.io.IOException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-public class DemoServlet extends HttpServlet {
-  @Override
-  public void doGet(HttpServletRequest req, HttpServletResponse resp)
-      throws IOException {
-    resp.setContentType("text/plain");
-    resp.getWriter().println("{ \"name\": \"World\" }");
-  }
+public class Stories extends HttpServlet
+{	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException
+	{	resp.setContentType("text/plain");
+    		resp.getWriter().println("{ \"name\": \"World\" }");
+		System.out.println("Welcome.\nThis is my cloud computing assignment. This app displays a collection of short stories from the website Daily Fiction.\n");
+		Scanner input = new Scanner(new File("stories.txt"));
+		while (input.hasNextLine())
+			System.out.println(input.nextLine());
+	}
 }
+
